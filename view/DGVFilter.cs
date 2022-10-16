cf5bd4e7a75eb468ebed83fa710189a672b55c9c
mcdodik20008 <100521935+mcdodik20008@users.noreply.github.com>
2022-10-16 10:18:54 +0500

    feat: добавил форму

---
+++ view/DGVFilter.cs
@@ -0,0 +1,62 @@
+﻿using DGVWF;
+using PISWF.infrasrtucture;
+using PISWF.infrasrtucture.auth.controller;
+using PISWF.infrasrtucture.auth.model.view;
+using PISWF.infrasrtucture.page;
+
+namespace PISWF.view;
+
+public class DGVFilter : Form
+{
+    private AuthController AuthController { get; }
+
+    private SourceFabric SourceFabric { get; }
+
+    private Page page = new(0, 25);
+
+    public DGVFilter(AuthController authController, SourceFabric sourceFabric)
+    {
+        AuthController = authController;
+        SourceFabric = sourceFabric;
+        InitializeItems();
+        AddControls();
+        
+        DG.DataSource = sourceFabric.GetDataTable(authController.ReadUser(page));
+    }
+
+    //Каждый метод обарачиваем в трукачте и при неудачи выкидываем пользователю масаге боксом или чем, то что не будет бесить.
+    private void createUser(object e, object sender)
+    {
+        AuthController.AddUser(new UserBasic(nameBox.Text, passwordBox.Text));
+        DG.DataSource = SourceFabric.GetDataTable(AuthController.ReadUser(page));
+    }
+
+    private void InitializeItems()
+    {
+        nameBox.Location = new Point(650, 100);
+        passwordBox.Location = new Point(650, 150);
+        CreateUserButton.Location = new Point(650, 200);
+        CreateUserButton.Click += createUser;
+        Size = new Size(900, 900);
+        DG.Bounds = new Rectangle(0, 0, 600, 600);
+        DG.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Bottom;
+        DG.AllowUserToAddRows = false;
+    }
+    
+    private void AddControls()
+    {
+        Controls.Add(DG);
+        Controls.Add(nameBox);
+        Controls.Add(passwordBox);
+        Controls.Add(CreateUserButton);
+    }
+
+    #region компоненты для формы
+
+    private DataGridViewWithFilter DG = new DataGridViewWithFilter();
+    private TextBox nameBox = new TextBox();
+    private TextBox passwordBox = new TextBox();
+    private Button CreateUserButton = new Button();
+
+    #endregion
+}
