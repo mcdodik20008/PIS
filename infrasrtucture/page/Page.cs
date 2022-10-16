a3ff863baef5a3dcdf7297260fcbb1bb058e1ecc
mcdodik20008 <100521935+mcdodik20008@users.noreply.github.com>
2022-10-16 10:17:36 +0500

    feat: пагинация

---
+++ infrasrtucture/page/Page.cs
@@ -0,0 +1,14 @@
+﻿namespace PISWF.infrasrtucture.page;
+
+public class Page
+{
+    public int Number { get; }
+    
+    public int Size { get; }
+
+    public Page(int number, int size)
+    {
+        Number = number;
+        Size = size;
+    }
+}
