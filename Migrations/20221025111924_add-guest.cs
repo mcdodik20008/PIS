eaa48e9af8f984adfa97f7dda7e324d5dc4ca468
mcdodik20008 <100521935+mcdodik20008@users.noreply.github.com>
2022-10-25 16:25:35 +0500

    fix: добавил миграцию на гостя

---
+++ Migrations/20221025111924_add-guest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PISWF.Migrations
+{
+    public partial class addguest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LogDate",
+                table: "log");
+            migrationBuilder.Sql("INSERT INTO \"user\" (login, \"password\") VALUES ('guest', 90120)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LogDate",
+                table: "log",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
