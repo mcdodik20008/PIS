c87b80b7bc2e536597d74064896d2bb1e15f4b41
Семен Бедрин <semyonbedrin@gmail.com>
2022-11-04 14:07:54 +0500

    feat: залил файлы для экспотра в эксель

--- infrasrtucture/ExcelExporter.cs
+++ infrasrtucture/ExcelExporter.cs
@@ -1,4 +1,5 @@
-﻿using OfficeOpenXml;
+﻿using System.Reflection;
+using OfficeOpenXml;
 
 namespace PISWF.infrasrtucture;
 
@@ -8,7 +9,22 @@ public class ExcelExporter
     {
         var package = new ExcelPackage();
         var type = typeof(T);
+        var props = type.GetProperties()
+            .Where(x => 
+                x.GetCustomAttribute(typeof(ToExcelAttribute)) is not null
+            ).ToArray();
         var sheet = package.Workbook.Worksheets.Add($"{type.Name} Report");
+        sheet.Cells[1, 1, 1, props.Length]
+            .LoadFromArrays(new object[][] { props.Select(x => x.Name).ToArray() });
+        var row = 2;
+        foreach (var item in data)
+        {
+            for (int i = 0; i < props.Length; i++)
+            {
+                sheet.Cells[row, i + 1].Value = props[i].GetValue(item);
+            }
+            row++;
+        }
         return package.GetAsByteArray();
     }
 }
