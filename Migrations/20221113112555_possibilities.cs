29348f32d545aba76f11e47abc286ce3899731f3
Семен Бедрин <semyonbedrin@gmail.com>
2022-11-13 19:10:39 +0500

    feat: добавил миграции на посибилити

---
+++ Migrations/20221113112555_possibilities.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PISWF.Migrations
+{
+    public partial class possibilities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "PossibilityId",
+                table: "role",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_role_PossibilityId",
+                table: "role",
+                column: "PossibilityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_role_visibility_PossibilityId",
+                table: "role",
+                column: "PossibilityId",
+                principalTable: "visibility",
+                principalColumn: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_role_visibility_PossibilityId",
+                table: "role");
+
+            migrationBuilder.DropIndex(
+                name: "IX_role_PossibilityId",
+                table: "role");
+
+            migrationBuilder.DropColumn(
+                name: "PossibilityId",
+                table: "role");
+        }
+    }
+}
---
+++ Migrations/20221113113351_posibility.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PISWF.Migrations
+{
+    public partial class posibility : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_role_visibility_PossibilityId",
+                table: "role");
+
+            migrationBuilder.CreateTable(
+                name: "possibility",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    rate = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_possibility", x => x.id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_role_possibility_PossibilityId",
+                table: "role",
+                column: "PossibilityId",
+                principalTable: "possibility",
+                principalColumn: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_role_possibility_PossibilityId",
+                table: "role");
+
+            migrationBuilder.DropTable(
+                name: "possibility");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_role_visibility_PossibilityId",
+                table: "role",
+                column: "PossibilityId",
+                principalTable: "visibility",
+                principalColumn: "id");
+        }
+    }
+}
