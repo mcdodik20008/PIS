f64a1e4c40203b4e09107b4a055810f175d8ca51
Семен Бедрин <semyonbedrin@gmail.com>
2022-11-15 10:26:08 +0500

    feat: добавил миграции

---
+++ Migrations/20221115052044_registerMcAddons.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PISWF.Migrations
+{
+    public partial class registerMcAddons : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "action_date",
+                table: "register-m-c",
+                type: "Date",
+                nullable: false,
+                defaultValue: new DateTime(2000, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "location",
+                table: "register-m-c",
+                type: "text",
+                nullable: false,
+                defaultValue: "test");
+
+            migrationBuilder.AddColumn<string>(
+                name: "omsu",
+                table: "register-m-c",
+                type: "text",
+                nullable: false,
+                defaultValue: "test");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "action_date",
+                table: "register-m-c");
+
+            migrationBuilder.DropColumn(
+                name: "location",
+                table: "register-m-c");
+
+            migrationBuilder.DropColumn(
+                name: "omsu",
+                table: "register-m-c");
+        }
+    }
+}
