2318b732bce0dac85d3da7d2f798e52c7558833e
Семен Бедрин <semyonbedrin@gmail.com>
2022-11-15 16:21:31 +0500

    feat: забыл миграции

---
+++ Migrations/20221115111241_registermc-string-number.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PISWF.Migrations
+{
+    public partial class registermcstringnumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "number",
+                table: "register-m-c",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "number",
+                table: "register-m-c",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
