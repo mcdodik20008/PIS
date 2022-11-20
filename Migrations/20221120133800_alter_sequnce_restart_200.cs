764457767bd82c3f3ca95ecd5dfa215f02acf0b3
mcdodik20008 <100521935+mcdodik20008@users.noreply.github.com>
2022-11-20 18:40:01 +0500

    feat: migration alter_sequence

---
+++ Migrations/20221120133800_alter_sequnce_restart_200.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PISWF.Migrations
+{
+    public partial class alter_sequnce_restart_200 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"ALTER SEQUENCE ""file_document_id_seq"" RESTART WITH 200;
+            ALTER SEQUENCE ""log_id_seq"" RESTART WITH 200;
+            ALTER SEQUENCE ""municipality_id_seq"" RESTART WITH 200;
+            ALTER SEQUENCE ""organization_id_seq"" RESTART WITH 200;
+            ALTER SEQUENCE ""possibility_id_seq"" RESTART WITH 200;
+            ALTER SEQUENCE ""register-m-c_id_seq"" RESTART WITH 200;
+            ALTER SEQUENCE ""role_id_seq"" RESTART WITH 200;
+            ALTER SEQUENCE ""visibility_id_seq"" RESTART WITH 200;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
